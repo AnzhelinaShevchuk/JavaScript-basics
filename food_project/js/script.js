5a4606cf4087ec8444bc34caa3fe70c7e6af785d
AnzhelinaShevchuk <42320349+AnzhelinaShevchuk@users.noreply.github.com>
2022-06-25 18:09:15 +0000

    used class for cards

--- food_project/js/script.js
+++ food_project/js/script.js
@@ -141,5 +141,68 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     }
     window.addEventListener("scroll", showModalByScroll);
+  
+//class
+
+    class MenuCard {
+      constructor(src, alt, title, descr, price, parentSelector) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.transfer = 27;
+        this.changeToUAH();
+      }
+  
+      changeToUAH() {
+        this.price = this.price * this.transfer;
+      }
+  
+      render() {
+        const element = document.createElement("div");
+        element.innerHTML = `
+           <div class="menu__item">
+                        <img src=${this.src} alt=${this.alt}>
+                        <h3 class="menu__item-subtitle">${this.title}</h3>
+                        <div class="menu__item-descr">${this.descr}</div>
+                        <div class="menu__item-divider"></div>
+                        <div class="menu__item-price">
+                            <div class="menu__item-cost">Цена:</div>
+                            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                        </div>
+                    </div>
+          `;
+        this.parent.append(element);
+      }
+    }
+  
+    new MenuCard(
+      "img/tabs/vegy.jpg",
+      "vegy",
+      'Menu "Fitness"',
+      'Menu "Fitness" is a new approach to cooking: more fresh vegetables and fruits. Product of active and healthy people. This is a brand new product with the best price and high quality!',
+      9,
+      ".menu .container"
+    ).render();
+  
+    new MenuCard(
+      "img/tabs/elite.jpg",
+      "elite",
+      'Menu "Premium"',
+      "Вn the “Premium” menu, we use not only beautiful packaging design, but also high-quality execution of dishes. Red fish, seafood, fruits - a restaurant menu without going to a restaurant!",
+      10,
+      ".menu .container"
+    ).render();
+  
+    new MenuCard(
+      "img/tabs/post.jpg",
+      "post",
+      "Lenten menu",
+      "he “Lenten” menu is a careful selection of ingredients: the complete absence of animal products, milk from almonds, oats, coconut or buckwheat, the right amount of protein from tofu and imported vegetarian steaks.",
+      11,
+      ".menu .container"
+    ).render();
   });
   
